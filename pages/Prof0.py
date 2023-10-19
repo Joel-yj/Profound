3fb10fe8eddcc1ad1def0509f31da8f69e293f7f
Joel Tan <78136208+Joel-yj@users.noreply.github.com>
2023-10-19 15:42:59 +0800

    Update Prof0.py

--- pages/Prof0.py
+++ pages/Prof0.py
@@ -3,23 +3,18 @@ import pandas as pd
 from st_pages import add_indentation, hide_pages, show_pages, Page, Section
 
 
-st.set_page_config(page_title="Profound", page_icon="🏠", layout="wide")
-hide_pages([
-    Page('profound.py'),
-    Page("Prof1.py")
-])
+st.set_page_config(layout="wide")
+
 add_indentation()
 
 df = pd.read_csv('Assignment2.csv')
 index = 0
 df = df.iloc[index]
-# st.image("dp/0.jpg")
-# html_str = f"""
-#             <h1 style = "text-align : center;" class="a">{df['Full Name']}</h1>
-#             """
-# st.markdown(html_str, unsafe_allow_html=True)
 
-st.title(df["Full Name"])
-st.image(f"dp/{index}.jpg",width=300)
+left_co, cent_co,last_co = st.columns([0.3,0.6,0.1])
+with cent_co:
+    st.title(df["Full Name"])
+    st.image(f"dp/{index}.jpg",width=300)
+
 st.header("Background Information")
 
