02df727add439ae85fb40a24326d948a243eef02
Joel Tan <nyjc39sc.joel@gmail.com>
2023-10-21 14:35:22 +0800

    updated prof profile page

--- pages/Prof0.py
+++ pages/Prof0.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 from st_pages import add_indentation, hide_pages, show_pages, Page, Section
+import ast
+from streamlit_extras.tags import tagger_component 
 
 
 st.set_page_config(layout="wide")
@@ -8,11 +10,14 @@ st.set_page_config(layout="wide")
 add_indentation()
 
 df = pd.read_csv('Assignment2.csv')
+df['Research Interest'] = df['Research Interest'].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else x)
 index = 0
 df = df.iloc[index]
 
 name = df['Full Name'].replace(" ", "_")
 pubs = pd.read_csv(f"publications/{name}.csv")
+
+pubs = pubs.drop(columns='Unnamed: 0')
 
 name = df['Full Name'] + ", Nanyang Technological University"
 citations_year = pd.read_csv(f"citations/{name}.csv")
@@ -31,6 +36,19 @@ with tab1:
 
 with tab2:
     st.header("Research Profile")
+
+    st.subheader("Research Interests")
+    tagger_component(
+        content= "",
+        tags = df['Research Interest'],
+        color_name="blue"
+    )
+
+    st.subheader("Publications")
     st.write(pubs)
-    st.bar_chart(citations_year, x = "Year", y = "Citations")
-    
+
+    st.subheader("Citations")
+
+    st.write("Citations by year")
+    st.bar_chart(citations_year, x = "Year", y = "Citations",)
+    st.write("Total citations: " + str(int(df['Citations'])))
--- pages/prof_compare.py
+++ pages/prof_compare.py
@@ -1,3 +1,9 @@
 import streamlit as st
+import pandas as pd
+from st_pages import add_indentation, hide_pages, show_pages, Page, Section
 
-st.title("Compare Prof")
+
+st.set_page_config(layout="wide")
+add_indentation()
+
+st.title("Compare Professors")
